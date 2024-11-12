e72342f7ba7cf7079ade1ddf9f8ae33b69f04d33
gdbmrm <>
2024-11-12 11:53:48 +0000

    modif indentation

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -2,7 +2,7 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
     getNumberOfDepartments() {
-        return Object.keys(employeesList).length;
-      },
+      return Object.keys(employeesList).length;
+    },
   };
 }
