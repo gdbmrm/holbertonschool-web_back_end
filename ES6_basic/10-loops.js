0db971b1025e4f73c76570de8282d2ec2a6715c1
gdbmrm <>
2024-11-12 12:00:36 +0000

    modif t10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,5 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   const myArray = [...array];
+
   for (const idx of myArray) {
     myArray[array.indexOf(idx)] = appendString + idx;
   }
