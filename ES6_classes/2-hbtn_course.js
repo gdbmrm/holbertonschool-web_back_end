21fd3fd996e08137e137424f02c55416ca7751be
gdbmrm <>
2024-11-12 14:33:40 +0000

    t2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,8 +1,17 @@
 export default class HolbertonCourse {
-    constructor(name, length, students) {
-        this.name = _name;
-        this.length = _length;
-        this.students = _students;
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
     }
+    this._name = name;
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = length;
 
+    if (!Array.isArray(students) || !students.every((students) => typeof students === 'string')) {
+      throw new TypeError('Student must be an array of string');
+    }
+    this._students = students;
+  }
 }
