4f5275723de3248227e1416653a29e7c4d77052c
gdbmrm <>
2024-11-13 11:33:14 +0000

    t2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-underscore-dangle */
 
-export default class HolbertonCourse {
+class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
