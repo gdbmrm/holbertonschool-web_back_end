8f08058df4185c5caf10b2d7dfafc3e793bb1457
gdbmrm <>
2024-11-21 14:21:40 +0000

    task 3 et 5

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -1,5 +1,3 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`))
-  });
+  return new Promise(new Error(`${filename} cannot be processed`));
 }
