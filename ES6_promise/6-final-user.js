990b8e32d3aa6c1d3893a605fcfcd479f149b772
gdbmrm <>
2024-11-21 14:04:29 +0000

    task 4, 5, 6, 7, 8, 9

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import { signUpUser } from './4-user-promise'
+import { uploadPhoto } from './5-photo-reject'
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const photo = uploadPhoto();
+  const user = signUpUser();
+
+  return Promise.allSettled({photo, user})
+    .then((results) => )
+}
