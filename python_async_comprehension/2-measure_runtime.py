c9c4fcb98dc4b7399369cb3fbb6dcaf93f19b064
gdbmrm <>
2024-12-14 23:10:48 +0100

    t2

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -7,8 +7,6 @@ measure_runtime should measure the total runtime and return it.
 """
 import asyncio
 import time
-from typing import float
-
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
