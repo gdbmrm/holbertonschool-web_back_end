fc23ea89f2866b8c5af08dc4e5177834687db8ab
gdbmrm <>
2025-01-23 15:51:01 +0000

    t4

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -3,6 +3,10 @@ const http = require('http');
 const app = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Holberton School!');
-}).listen(1245);
+});
+
+app.listen(1245, () => {
+    console.log('Server is running on http://locoalhost:1245');
+  });
 
 module.exports = app;
