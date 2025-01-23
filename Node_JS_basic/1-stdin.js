f01d64437a6f89cec4004b0c6242d245f3f98417
gdbmrm <>
2025-01-23 13:42:24 +0000

    task 0 et 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,22 +1,8 @@
-process.stdin.setEncoding('utf8');
+process.stdout.write("Welcome to Holberton School, what is your name?\n")
 
-console.log("Welcome to Holberton School, what is your name?")
-
-
-let studentName;
-
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
-  if (chunk !== null) {
-    studentName = chunk
-  } else {
-    studentName = "Bob"
-  }
+process.stdin.on('data', data => { 
+  process.stdout.write(`Your name is: ${data}`)
+  process.stdout.write("This important software is now closing\n");
+  process.exit(); 
 });
 
-process.stdin.on('end', function() {
-  process.stdout.write('end');
-});
-
-console.log(`Your name is: ${studentName}`)
-console.log("This important software is now closing")
