ae781f879a9ce186cd34f73bcafe60ee19a9a668
gdbmrm <mgoudiaby057@gmail.com>
2025-01-27 00:07:37 +0100

    t1 t2 t3

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
-process.stdin.setEncoding('utf8');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.on('data', data => {
-  process.stdout.write(`Your name is: ${data}`);
+rl.on('line', (name) => {
+  process.stdout.write(`Your name is: ${name}\n`);
   process.stdout.write('This important software is now closing\n');
-  process.exit();
+  rl.close();
 });
--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,11 +1,26 @@
 function countStudents(path) {
   try {
-    const data = fs.readFileSync(path, "utf-8");
-    console.log(`Number of students: ${nb_of_students}`)
+    const data = fs.readFileSync(path);
+    const content = data.toString('utf-8').split('\n');
+    const lines = content.slice(1);
+    const studentsData = content.split(',')
+
+    if (lines.length < 2) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = {};
+    const totalStudents = lines.length;
+    console.log(`Number of students: ${totalStudents}`)
+
+    studentsData.forEach((line) => {
+
+      
+    });
+
+    console.log(`Number of students in FIELD: ${totalStudents}. List: ${students}`)
 
   } catch (error) {
     throw new error ('Cannot load the database');
   }
-  
-
 }
